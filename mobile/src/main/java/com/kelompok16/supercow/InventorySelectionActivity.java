716acca37b7df7d49b7bd67a7289925bfb871f89
13514103 muhammadreifiza <13514103@std.stei.itb.ac.id>
2017-02-20 21:43:37 +0700

    Initial Plan For UI Component

---
+++ mobile/src/main/java/com/kelompok16/supercow/InventorySelectionActivity.java
@@ -0,0 +1,13 @@
+package com.kelompok16.supercow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InventorySelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inventory_selection);
+    }
+}
